7b2b937cd8466fa623afae4ac773aba12739d623
nagielhachem <nagielhachem@gmail.com>
2017-11-14 10:53:38 +0100

    portfolio methods can be called without dates

--- src/finance/portfolio.cpp
+++ src/finance/portfolio.cpp
@@ -21,13 +21,46 @@ void Portfolio::set_assets(std::vector<std::tuple<Asset*, float>> assets)
                               [](float s, std::tuple<Asset*, float> a) -> float {
                                 return s + std::get<1>(a);
                               });
-
-  std::cout << sum << std::endl;
   float eps = 0.01;
   if (sum >= 1 - eps && sum <= 1 + eps)
     this->assets = assets;
   else
     throw std::invalid_argument("Weights must sum to 1");
+}
+
+void Portfolio::set_weights(std::vector<float> weights)
+{
+  float sum = std::accumulate(weights.begin(), weights.end(), 0.0,
+                              [](float s, float w) -> float {
+                                return s + w;
+                              });
+  float eps = 0.01;
+  if (sum >= 1 - eps && sum <= 1 + eps)
+    for (size_t i = 0; i != weights.size(); ++i)
+      std::get<1>(this->assets[i]) = weights[i];
+  else
+    throw std::invalid_argument("Weights must sum to 1");
+}
+
+
+std::vector<float> Portfolio::get_returns(hlp::Date start_date, hlp::Date end_date) const
+{
+  std::vector<float> returns(this->assets.size());
+  std::transform(this->assets.begin(), this->assets.end(), returns.begin(),
+                [&start_date, &end_date](std::tuple<Asset*, float> a) -> float {
+                  return std::get<0>(a)->get_return(start_date, end_date);
+                });
+  return returns;
+}
+
+std::vector<float> Portfolio::get_returns() const
+{
+  std::vector<float> returns(this->assets.size());
+  std::transform(this->assets.begin(), this->assets.end(), returns.begin(),
+                [](std::tuple<Asset*, float> a) -> float {
+                  return std::get<0>(a)->get_return();
+                });
+  return returns;
 }
 
 float Portfolio::get_return(hlp::Date start_date, hlp::Date end_date) const
@@ -36,6 +69,18 @@ float Portfolio::get_return(hlp::Date start_date, hlp::Date end_date) const
   std::transform(this->assets.begin(), this->assets.end(), returns.begin(),
                 [&start_date, &end_date](std::tuple<Asset*, float> a) -> float {
                   float ret = std::get<0>(a)->get_return(start_date, end_date);
+                  float w = std::get<1>(a);
+                  return w * ret;
+                });
+  return std::accumulate(returns.begin(), returns.end(), 0.0);
+}
+
+float Portfolio::get_return() const
+{
+  std::vector<float> returns(this->assets.size());
+  std::transform(this->assets.begin(), this->assets.end(), returns.begin(),
+                [](std::tuple<Asset*, float> a) -> float {
+                  float ret = std::get<0>(a)->get_return();
                   float w = std::get<1>(a);
                   return w * ret;
                 });
@@ -56,6 +101,20 @@ float Portfolio::get_volatility(hlp::Date start_date, hlp::Date end_date) const
   return sqrtf(vol);
 }
 
+float Portfolio::get_volatility() const
+{
+  float vol = 0;
+  std::vector<float> cov = this->get_covariance();
+  for (std::size_t i = 0; i != this->assets.size(); ++i)
+    for (std::size_t j = 0; j != this->assets.size(); ++j)
+    {
+      float wi = std::get<1>(this->assets[i]);
+      float wj = std::get<1>(this->assets[j]);
+      vol += wi * wj * cov[i * this->assets.size() + j];
+    }
+  return sqrtf(vol);
+}
+
 std::vector<float> Portfolio::get_covariance(hlp::Date start_date, hlp::Date end_date) const
 {
   std::vector<float> covariance(this->assets.size() * this->assets.size());
@@ -65,6 +124,27 @@ std::vector<float> Portfolio::get_covariance(hlp::Date start_date, hlp::Date end
     {
       std::vector<float> ri = std::get<0>(this->assets[i])->get_returns(start_date, end_date);
       std::vector<float> rj = std::get<0>(this->assets[j])->get_returns(start_date, end_date);
+      float ri_avg = std::accumulate(ri.begin(), ri.end(), 0.0) / ri.size();
+      float rj_avg = std::accumulate(rj.begin(), rj.end(), 0.0) / rj.size();
+
+      std::size_t ind = i * this->assets.size() + j;
+      covariance[ind] = 0;
+      for (std::size_t k = 0; k != ri.size(); ++k)
+        covariance[ind] += (ri[k] - ri_avg) * (rj[k] - rj_avg);
+      covariance[ind] /= ri.size();
+    }
+  return covariance;
+}
+
+std::vector<float> Portfolio::get_covariance() const
+{
+  std::vector<float> covariance(this->assets.size() * this->assets.size());
+
+  for (std::size_t i = 0; i != this->assets.size(); ++i)
+    for (std::size_t j = 0; j != this->assets.size(); ++j)
+    {
+      std::vector<float> ri = std::get<0>(this->assets[i])->get_returns();
+      std::vector<float> rj = std::get<0>(this->assets[j])->get_returns();
       float ri_avg = std::accumulate(ri.begin(), ri.end(), 0.0) / ri.size();
       float rj_avg = std::accumulate(rj.begin(), rj.end(), 0.0) / rj.size();
 
--- src/finance/portfolio.hpp
+++ src/finance/portfolio.hpp
@@ -14,11 +14,19 @@ public:
 
   std::vector<std::tuple<Asset*, float>> get_assets() const;
   void set_assets(std::vector<std::tuple<Asset*, float>> assets);
+  void set_weights(std::vector<float> weights);
+
+  std::vector<float> get_returns(hlp::Date start_date, hlp::Date end_date) const;
+  std::vector<float> get_returns() const;
 
   float get_return(hlp::Date start_date, hlp::Date end_date) const;
-  float get_volatility(hlp::Date start_date, hlp::Date end_date) const;
+  float get_return() const;
 
   std::vector<float> get_covariance(hlp::Date start_date, hlp::Date end_date) const;
+  std::vector<float> get_covariance() const;
+
+  float get_volatility(hlp::Date start_date, hlp::Date end_date) const;
+  float get_volatility() const;
 
 private:
   std::vector<std::tuple<Asset*, float>> assets;

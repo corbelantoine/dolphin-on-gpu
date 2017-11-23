cd0077dad565465f237f6c9268a974d7a0b08360
Nagi El Hachem <nagi.elhachem@epita.fr>
2017-11-23 17:40:28 +0100

    adapted asset to gpu. still have warnings...

--- src/finance/asset.cu
+++ src/finance/asset.cu
@@ -4,8 +4,6 @@
 #include <numeric>
 #include <algorithm>
 
-#include <cstdio>
-#include <cstdlib>
 
 namespace fin
 {
@@ -27,7 +25,7 @@ void Asset::set_closes(std::vector<Close> closes)
   // sort closes by date
   this->sort_closes(closes);
   // set closes size
-  this->size = this->closes.size();
+  this->size = closes.size();
   // allocate closes
   this->closes = new Close[this->size];
   // set closes
@@ -40,9 +38,11 @@ Close* Asset::get_closes() const
   return this->closes;
 }
 
-Close* Asset::get_closes(hlp::Date start_date, hlp::Date end_date, int *n) const
+__host__ __device__ Close* Asset::get_closes(hlp::Date start_date, hlp::Date end_date,
+        int *n, bool gpu) const
 {
   int start, end;
+  // get period (start -> end)
   for(int i = 0; i < this->size; ++i) {
     if (this->closes[i].date == start_date)
       start = i;
@@ -51,8 +51,14 @@ Close* Asset::get_closes(hlp::Date start_date, hlp::Date end_date, int *n) const
       continue;
     }
   }
+  // set size of return array
   *n = end - start;
-  Close closes[*n];
+  // allocate memory for closes depending on gpu/cpu
+  Close* closes;
+  if (gpu)
+    cudaMalloc((void **) &closes, sizeof(Close) * *n);
+  else
+    closes = new Close[*n];
   for (int i = 0; i < *n; ++i)
     closes[i] = this->closes[i + start];
   return closes;
@@ -78,42 +84,57 @@ float Asset::get_return(hlp::Date start_date, hlp::Date end_date) const
   return (v2 - v1) / v1;
 }
 
-float* Asset::get_returns(int *n) const
+__host__ __device__ float* Asset::get_returns(int *n, bool gpu) const
 {
   *n = this->size - 1;
-  // compute all dayly returns on that period
-  float returns[*n];
+  // allocate memory for returns
+  float* returns;
+  if (gpu)
+    cudaMalloc((void **) &returns, sizeof(float) * *n);
+  else
+    returns = new float[*n];
+  // compute all daily returns on that period
   for (int i = 0; i < *n; ++i) {
-    float v1 = this->closes[i].value;
-    float v2 = this->closes[i + 1].value;
-    returns[i] = (v2 - v1) / v1;
+      float v1 = this->closes[i].value;
+      float v2 = this->closes[i + 1].value;
+      returns[i] = (v2 - v1) / v1;
   }
-
+  
   return returns;
 }
 
-__host__ __device__ float* Asset::get_returns(hlp::Date start_date, hlp::Date end_date, int* n) const
+__host__ __device__ float* Asset::get_returns(hlp::Date start_date, hlp::Date end_date,
+        int* n, bool gpu) const
 {
   // get asset closes on this period (start->end)
   Close* closes = this->get_closes(start_date, end_date, n);
   // set n to returns size (closes - 1: it's dayly return)
   *n -= 1;
-  // compute all dayly returns on that period
-  float returns[*n];
+  // allocate memory for returns
+  float* returns;
+  if (gpu)
+    cudaMalloc((void **) &returns, sizeof(float) * *n);
+  else
+    returns = new float[*n];
+  // compute all daily returns on that period
   for (int i = 0; i < *n; ++i) {
     float v1 = closes[i].value;
     float v2 = closes[i + 1].value;
     returns[i] = (v2 - v1) / v1;
   }
-
+  // free memory reserved for closes
+  if (gpu)
+    cudaFree(closes);
+  else
+    delete[] closes;
   return returns;
 }
 
-float Asset::get_volatility() const
+__host__ __device__ float Asset::get_volatility(bool gpu) const
 {
   int n;
-  // get dayly returns
-  float* returns = this->get_returns(&n);
+  // get daily returns
+  float* returns = this->get_returns(&n, gpu);
   // compute average return
   float avg = 0;
   for (int i = 0; i < n; ++i)
@@ -124,15 +145,20 @@ float Asset::get_volatility() const
   for (int i = 0; i < n; ++i)
     var += pow(returns[i] - avg, 2);
   var /= n;
+  // free memory of daily returns
+  if (gpu)
+    cudaFree(returns);
+  else
+    delete[] returns;
   // return volatility: sqrt(variance)
   return sqrtf(var);
 }
 
-float Asset::get_volatility(hlp::Date start_date, hlp::Date end_date) const
+__host__ __device__ float Asset::get_volatility(hlp::Date start_date, hlp::Date end_date, bool gpu) const
 {
   int n;
   // get dayly returns
-  float* returns = this->get_returns(start_date, end_date, &n);
+  float* returns = this->get_returns(start_date, end_date, &n, gpu);
   // compute average return
   float avg = 0;
   for (int i = 0; i < n; ++i)
@@ -143,6 +169,11 @@ float Asset::get_volatility(hlp::Date start_date, hlp::Date end_date) const
   for (int i = 0; i < n; ++i)
     var += pow(returns[i] - avg, 2);
   var /= n;
+  // free memory of daily returns
+  if (gpu)
+    cudaFree(returns);
+  else
+    delete[] returns;
   // return volatility: sqrt(variance)
   return sqrtf(var);
 }
--- src/finance/asset.hpp
+++ src/finance/asset.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
+#include <cstdio>
+#include <cstdlib>
+
 #include <vector>
+
 #include "../helpers/date.hpp"
 
 namespace fin
@@ -16,19 +20,22 @@ class Asset
 {
 public:
   Asset(int id);
-  ~Asset() = default;
+  ~Asset();
 
   void set_closes(std::vector<Close> closes);
 
   Close* get_closes() const;
-  Close* get_closes(hlp::Date start_date, hlp::Date end_date, int *n) const;
+  Close* get_closes(hlp::Date start_date, hlp::Date end_date, 
+          int *n, bool gpu = false) const;
 
   float get_return() const;
   float get_return(hlp::Date start_date, hlp::Date end_date) const;
-  float* get_returns(int* n) const;
-  float* get_returns(hlp::Date start_date, hlp::Date end_date, int* n) const;
-  float get_volatility() const;
-  float get_volatility(hlp::Date start_date, hlp::Date end_date) const;
+  float* get_returns(int* n, bool gpu = false) const;
+  float* get_returns(hlp::Date start_date, hlp::Date end_date,
+          int* n, bool gpu = false) const;
+  float get_volatility(bool gpu = false) const;
+  float get_volatility(hlp::Date start_date, hlp::Date end_date, 
+          bool gpu = false) const;
 
 private:
   int id;
